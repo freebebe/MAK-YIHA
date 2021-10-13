40acab03fb0bff317ab82dd2f9fc09a36c39fca9
freebebe <gogofreebebe@tutamail.com>
2021-10-13 16:38:27 +1100

    test

---
+++ trip-mate/src/components/App.jsx
@@ -0,0 +1,86 @@
+import React, { Component } from "react";
+
+// components
+import { addTrip } from "./trips/addTrip";
+
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      allTrips: [
+        {
+          place: "m1",
+          date: "2020-01-01",
+          type: "tt",
+        },
+        {
+          place: "m2",
+          date: "2020-01-01",
+          type: "tt2",
+        },
+        {
+          place: "m3",
+          date: "2020-01-01",
+          type: "tt3",
+        },
+      ],
+    };
+    this.addTrip = this.addTrip.bind(this);
+  }
+
+  addTrip(newTrip) {
+    this.setState((prevState) => {
+      return {
+        allTrips: [
+          ...prevState.allTrips,
+          newTrip,
+        ],
+      };
+    });
+  }
+  countDays(filter) {
+    const { allTrips } = this.state;
+    return allTrips.filter((trip) => filter ? trip.type === filter : trip)
+      .length;
+  }
+
+  // swich goes to first matching route
+
+  render() {
+    return (
+      <div className="app">
+        <Router>
+          <div className="route-container">
+            <Nav />
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={(props) => {
+                  <TripsCount
+                    {...props}
+                    total={this.countDays()}
+                    tt={this.countDays("mm1")}
+                    tt2={this.countDays("mm2")}
+                    tt3={this.countDays("mm3")}
+                  />;
+                }}
+              >
+              </Route>
+              <Route
+                path="/list/:filer"
+                render={(props) => {
+                  <TripsList
+                    {...props}
+                    days={this.state.allTrips}
+                  />;
+                }}
+              >
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    );
+  }
+}
---
+++ trip-mate/src/components/trips/AddTrip.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+export const AddTrip = ({ newTrip }) => {
+  let date, place, type;
+  const submit = (e) => {
+    e.preventDefault();
+    newTrip({
+      date: date.value,
+      place: place.value,
+      type: type.value,
+    });
+    date.value = place.value = type.value = "";
+  };
+  return (
+    <div className="form-container">
+      <from className="form black-container">
+        <label
+          htmlFor="
+            "
+        >
+          <h3></h3>
+          <br />
+          <br />
+          <br />
+        </label>
+        <label
+          htmlFor="
+    "
+        >
+          Date: <br />
+          <input id="date" type="date" required ref={(input) => date = input} />
+        </label>
+        <label htmlFor="">
+          Place: <br />
+          <input
+            id="date"
+            type="text"
+            required
+            ref={(input) => place = input}
+          />
+        </label>
+        <label htmlFor="">
+          Type: <br />
+          <select ref={(input) => type = input}>
+            <option value="tt"></option>
+            <option value="tt2"></option>
+            <option value="tt3"></option>
+          </select>
+        </label>
+        <input type="submit" value="SUBMIT" />
+      </from>
+    </div>
+  );
+};
