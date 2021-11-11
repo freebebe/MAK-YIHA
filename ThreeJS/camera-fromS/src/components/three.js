4b041f5e88118f11bdb19a3fd6d6bbff023dadcf
freebebe <gogofreebebe@tutamail.com>
2021-11-11 04:20:34 -0500

    b2vie

---
+++ ThreeJS/camera-fromS/src/components/three.js
@@ -0,0 +1,32 @@
+// https://medium.com/geekculture/how-to-control-three-js-camera-like-a-pro-a8575a717a2
+import * as Three from "three";
+
+import { FlyControls } from "../node_modules/three/examples/jsm/controls/FlyControls";
+import { addRoom, addWallLighting } from "./components/common";
+
+let camera,
+  scene,
+  canvas,
+  controls,
+  renderer;
+
+init();
+animate();
+
+function init() {
+  scene = new THREE.Scene();
+  canvas = document.querySelector("#canvas");
+
+  renderer = new THREE.WebGLRenderer({ canvas });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  camera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    1,
+    5000,
+  );
+  camera.position.x = 0;
+  camera.position.y = 75;
+  camera.position.z = 16;
+}
---
+++ ThreeJS/camera-fromS/src/main.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <demo1 />,
+  document.getElementById('demo1')
+)
